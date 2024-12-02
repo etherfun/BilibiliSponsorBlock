c912b77c8e0b13004107e0e30a2594fe8a443a6a
etherfun <124044584+etherfun@users.noreply.github.com>
2024-12-02 21:27:54 +0800

    change to XMLHttpRequest

--- src/document.ts
+++ src/document.ts
@@ -11,24 +11,40 @@ const sendMessageToContent = (messageData: InjectedScriptMessageSend, payload): 
         "/"
     );
 };
-let frameRate: number = 30;
+interface Video {
+    id: number;
+    frameRate: number;
+}
+let VideoData: { data: { dash: { video: Video[] } } };
+
 
 (function () {
-    const originalFetch = window.fetch;
+    const originalOpen = XMLHttpRequest.prototype.open;
+    const originalSend = XMLHttpRequest.prototype.send;
 
-    window.fetch = async function (input, init) {
-        const url = typeof input === 'string' ? input : (input as Request).url;
+    XMLHttpRequest.prototype.open = function (
+        method: string,
+        url: string | URL,
+        async: boolean = true,
+        user?: string | null,
+        password?: string | null
+    ) {
+        this._url = url.toString();
+        originalOpen.call(this, method, url, async, user, password);
+    };
 
-        const response = await originalFetch(input, init);
-        if (url.includes('/player/wbi/playurl') && url.includes(window.__INITIAL_STATE__.cid.toString())) {
-            response.clone().json().then(data => {
-                frameRate = data.data.dash.video
-                    .filter((v) => v.id === data.data.quality && v.codecid === data.data.video_codecid)[0]?.frameRate;
-            }).catch(() => {
-                frameRate = 30;
-            });
-        }
-        return response;
+    XMLHttpRequest.prototype.send = function (body?: Document | BodyInit | null) {
+        this.addEventListener('readystatechange', function () {
+            if (this.readyState === 4 && this.status >= 200 && this.status < 300) {
+                const url = this._url;
+
+                if (url.includes('/player/wbi/playurl') && url.includes(window.__INITIAL_STATE__.cid.toString())) {
+                        VideoData = JSON.parse(this.responseText);
+                }
+            }
+        });
+
+        originalSend.call(this, body);
     };
 })();
 
@@ -41,6 +57,14 @@ function windowMessageListener(message: MessageEvent) {
         if (data.type === "getBvID") {
             sendMessageToContent(data, window?.__INITIAL_STATE__?.bvid);
         } else if (data.type === "getFrameRate") {
+            let frameRate: number;
+            try {
+                frameRate = VideoData.data.dash.video
+                .filter((v: { id: number }) => v.id === JSON.parse(window.localStorage.bpx_player_profile).media.quality)[0].frameRate;
+            }catch (e) {//初始化的第一次调用会比初始化xhr请求快,返回默认值
+                frameRate = 30;
+            }
+            console.log(frameRate);
             sendMessageToContent(data, frameRate);
         } else if (data.type === "getChannelID") {
             sendMessageToContent(data, window?.__INITIAL_STATE__?.upData?.mid);
