a4b2611347e09d3cccbd35b1fca219cb91b38fc5
hanyd <hanyd@bsbsb.top>
2026-03-22 10:08:51 +0800

    read aid from player manifest

--- src/document/aidMap.ts
+++ src/document/aidMap.ts
@@ -23,6 +23,10 @@ export async function getBvid(aid: number | string): Promise<BVID> {
     if (window?.__INITIAL_STATE__?.aid && window.__INITIAL_STATE__.bvid) {
         saveAid(window.__INITIAL_STATE__.aid, window.__INITIAL_STATE__.bvid);
     }
+    const manifest = window?.player?.getManifest()
+    if (manifest != null && manifest.aid != null && manifest.bvid != null) {
+        saveAid(manifest.aid, manifest.bvid);
+    }
     if (!cache.contains(aid as AID)) {
         let bvid: BVID;
         try {
